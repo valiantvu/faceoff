f93e3882556f167a80fd972b1c883fb1ebc895e0
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-15 23:22:29 -0700

    Add threads view

--- www/components/thread/thread.js
+++ www/components/thread/thread.js
@@ -2,8 +2,21 @@ angular.module('faceoff.thread', [
 	'ionic'
 	])
 
-.controller('ThreadController', function($scope, thread) {
-
-	$scope.thread = [thread]; // thread is only a single name for now
-
+.controller('ThreadController', function($scope, thread, Camera, $timeout) {
+  console.log("THREAD " ,thread)
+	$scope.thread = thread; // current user is the first object in array
+  $scope.photos = [];
+  for (var i = 0; i < 4; i++) {
+    Camera.getRandomPicture().then(function(imageURL) {
+      var random = Math.floor(Math.random() * 1000);
+      var photoObj = {};
+      photoObj.photoURL = imageURL;
+      photoObj.createdAt = new Date(new Date() - random*1000000);
+      photoObj.owner = $scope.thread[random%2]
+      $scope.photos.push(photoObj);
+    })
+  }
+  $timeout(function() {
+    console.log('THREAD PHOTOS: ', $scope.photos);
+  },200)
 });
