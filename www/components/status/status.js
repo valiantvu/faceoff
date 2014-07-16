79daf81914dcb51e4cd79c6e0782a5ef61e356fa
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-16 13:22:35 -0700

    Test CORS API call

--- www/components/status/status.js
+++ www/components/status/status.js
@@ -3,7 +3,7 @@ angular.module('faceoff.status', [
 	'services'
 	])
 
-.controller('StatusController', function($scope, $state, threads, ThreadsService) {
+.controller('StatusController', function($scope, $state, threads, ThreadsService, $http) {
 
 	console.log("THREADS ", threads);
 	$scope.threads = threads;
@@ -12,5 +12,15 @@ angular.module('faceoff.status', [
 		ThreadsService.setSelected(thread);
 		$state.go('thread');
 	}
+
+  $scope.clickMe = function() {
+    $http.get("http://tradingfaces.herokuapp.com/api/users")
+              .success(function(data) {
+                console.log(data);
+              })
+              .error(function(error) {
+                console.log(error);
+              })
+  }
 
 });
