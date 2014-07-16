c32269c8c451153a5f1c4c17e061166e6238553d
TheDevilsKode <mrwebbin@gmail.com>
2014-07-16 12:52:28 -0700

    Add a pre processing functon for thread create

--- server/api/thread/thread.controller.js
+++ server/api/thread/thread.controller.js
@@ -5,6 +5,8 @@ var passport = require('passport');
 var config = require('../../config/environment');
 var jwt = require('jsonwebtoken');
 var Photo = require('../photo/photo.model');
+var User = require('../user/user.model');
+var Q = require('q');
 
 var validationError = function(res, err) {
   return res.json(422, err);
@@ -22,29 +24,85 @@ exports.index = function(req, res) {
 };
 
 /**
+ * Loops through an array of users phone numbers.
+ * Replaces a user's phone number with his/her ObjectId if user is registered,
+ * otherwise it creates a user object and replaces the user's phone number with
+ * the newly created ObjectId
+ */
+
+var preCreate = function(participants){
+  //Input: an array of participants phone numbers
+  //Output: an array of participants user ids
+  //BEGIN
+    // set promiseArray to []
+    // loop participant in array
+      // create and empty promise
+      // lookup phone number
+      // if user exists then
+        // resolve promise with user id
+      // else 
+        // create new user object
+        // save new user object
+        // resolve promise with new user id
+      // add promise to promiseArray
+    // return promise array
+  //END
+  var promiseArray = [];
+  participants.forEach(function(participant){
+    var deferred = Q.defer();
+    User.findOne({"phone": participant}, function(err, user){
+      //bubble up errors
+      if(err) deferred.reject(err);
+      if(user){
+        deferred.resolve(user._id);
+      }else{
+        var newUser = new User({
+          "first": "Pending",
+          "last": "Pending",
+          "phone": participant
+        });
+        newUser.save(function(err, user){
+          // bubble up errors
+          if(err) deferred.reject(err);
+          deferred.resolve(newUser._id);
+        });
+      }
+    })
+
+    promiseArray.push(deferred.promise);
+  })
+  return promiseArray;
+};
+
+/**
  * Creates a new thread
  * Expects the following parameters:
  * req.body.participants -> Expect >= 2 participants -> ex: [ObjectId, ObjectId]
  * req.body.url -> Photo url -> ex: String
  * req.body.owner -> Photo owner id -> ex: String
  */
-
-
 ///{particpants:{id: 53c3fc714dbca9cb1589e695, id:53c3fc714dbca9cb1589e696}, owner:53c3fc714dbca9cb1589e696, url: http://goo.gl/oUgHgn}
 exports.create = function (req, res, next) {
-  console.log(req.body);
-  var newThread = new Thread({participants: req.body.participants});
-  newThread.save(function(err, thread) {
-    if (err) return validationError(res, err);
-    var newPhoto = new Photo({url: req.body.url, owner: req.body.owner});
-    newPhoto.save(function(err, photo) {
+  var promises = preCreate(req.body.participants);
+  Q.all(promises).then(function(result){
+    req.body.participants = result;
+    var newThread = new Thread({participants: req.body.participants});
+    newThread.save(function(err, thread) {
       if (err) return validationError(res, err);
-      thread.photos.push(photo.id);
-      thread.save(function(err, updatedThread) {
+      var newPhoto = new Photo({url: req.body.url, owner: req.body.owner});
+      newPhoto.save(function(err, photo) {
         if (err) return validationError(res, err);
-        res.json({ data: updatedThread });
+        thread.photos.push(photo.id);
+        thread.save(function(err, updatedThread) {
+          if (err) return validationError(res, err);
+          res.json({ data: updatedThread });
+        });
       });
     });
+  }, function(err){
+    console.log(err);
+    // Letting client know of an error, might want to send back more detail
+    res.status(500).send(err);
   });
 };
 
@@ -128,3 +186,4 @@ exports.me = function(req, res, next) {
 exports.authCallback = function(req, res, next) {
   res.redirect('/');
 };
+
