1c77b4f8b1032bb0c3f308dde05704beae23fdaf
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-16 13:21:53 -0700

    Set server to allow CORS

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,14 @@ var errors = require('./components/errors');
 
 module.exports = function(app) {
 
+  // Enable CORS header for all incoming requests
+  app.use('/*',function(req,res,next){
+      res.header({
+        'Access-Control-Allow-Origin': '*'
+      });
+      next();
+  });
+
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
