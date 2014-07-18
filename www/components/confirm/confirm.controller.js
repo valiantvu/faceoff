2f441326fcf41d0f8c3376049c3b8743e5638537
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-17 17:35:55 -0700

    Refactor confirm controller logic

--- www/components/confirm/confirm.controller.js
+++ www/components/confirm/confirm.controller.js
@@ -30,51 +30,37 @@ angular.module('faceoff.confirmthread', [
 
         }
         else {
-          API.newThread([$scope.user.phone, $scope.friends.phone])
-            .success(function(newThread) {
-              console.log(newThread);
-              var threadId = newThread.data._id;
-              var ownerId = newThread.data.participants[0];
-              // Remove this line when we have real photos to send.
-              Camera.getRandomPicture().then(function(image) {
-                API.newPhoto(threadId, ownerId, image)
-                  .success(function(data) {
-                    $state.go('menu.status');
-                  })
-                  .error(function(error) {
-                    console.log(error);
-                  });
-              })
-            })
-            .error(function(error) {
-              console.log(error);
-            })
+          createNewThread();
         }
       })
       
     }
     else {
-      API.newThread([$scope.user.phone, $scope.friends.phone])
-        .success(function(newThread) {
-          console.log(newThread);
-          var threadId = newThread.data._id;
-          var ownerId = newThread.data.participants[0];
-          // Remove this line when we have real photos to send.
-          Camera.getRandomPicture().then(function(image) {
-            API.newPhoto(threadId, ownerId, image)
-              .success(function(data) {
-                $state.go('menu.status');
-              })
-              .error(function(error) {
-                console.log(error);
-              });
-          })
-        })
-        .error(function(error) {
-          console.log(error);
-        })
+      createNewThread();
     }
   };
+
+  var createNewThread = function() {
+    API.newThread([$scope.user.phone, $scope.friends.phone])
+      .success(function(newThread) {
+        console.log(newThread);
+        var threadId = newThread.data._id;
+        var ownerId = newThread.data.participants[0];
+        // Remove this line when we have real photos to send.
+        Camera.getRandomPicture().then(function(image) {
+          API.newPhoto(threadId, ownerId, image)
+            .success(function(data) {
+              $state.go('menu.status');
+            })
+            .error(function(error) {
+              console.log(error);
+            });
+        })
+      })
+      .error(function(error) {
+        console.log(error);
+      })
+  }
 
   var init = function() {
     // Search database for the friend using their phone number.
